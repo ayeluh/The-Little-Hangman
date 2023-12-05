2b259011ef18a67c9e02dffe7347802adf3f20b2
angelannecantano <152976049+angelannecantano@users.noreply.github.com>
2023-12-05 19:18:08 +0800

    Add files via upload

---
+++ The Little Hangman - Studi Buddi v2/script text/word box.js
@@ -0,0 +1,202 @@
+const wordBox = [
+    {
+        word: "amiable",
+        clue: "Friendly and pleasant in disposition."
+    },
+    {
+        word: "cacophony ",
+        clue: "A harsh, discordant mixture of sounds."
+    },
+    {
+        word: "debilitate",
+        clue: "To make someone weak or infirm."
+    },
+    {
+        word: "ephemeral",
+        clue: "Lasting for a very short time."
+    },
+    {
+        word: "facetious",
+        clue: "Treating serious issues with inappropriate humor."
+    },
+    {
+        word: "garrulous",
+        clue: "Excessively talkative, especially on trivial matters."
+    },
+    {
+        word: "hapless",
+        clue: "Unfortunate or unlucky."
+    },
+    {
+        word: "idiosyncrasy",
+        clue: "A distinctive or peculiar feature or characteristic of an individual."
+    },
+    {
+        word: "juxtapose",
+        clue: "To place or deal with close together for contrasting effect."
+    },
+    {
+        word: "kaleidoscope",
+        clue: "A constantly changing pattern or sequence of elements."
+    },
+    {
+        word: "languish",
+        clue: "To grow weak or feeble; to suffer neglect."
+    },
+    {
+        word: "munificent",
+        clue: "Very generous; more generous than is usual or necessary."
+    },
+    {
+        word: "nefarious",
+        clue: "Wicked, villainous, or criminal."
+    },
+    {
+        word: "obliterate",
+        clue: "To destroy utterly; wipe out."
+    },
+    {
+        word: "pernicious",
+        clue: "Having a harmful effect, especially in a gradual or subtle way."
+    },
+    {
+        word: "quell",
+        clue: "To suppress, subdue, or extinguish."
+    },
+    {
+        word: "reverie",
+        clue: "A state of being pleasantly lost in one's thoughts."
+    },
+    {
+        word: "sycophant",
+        clue: "A person who acts obsequiously towards someone important in order to gain advantage."
+    },
+    {
+        word: "trepidation",
+        clue: "A feeling of fear or agitation about something that may happen."
+    },
+    {
+        word: "ubiquitous",
+        clue: "Present, appearing, or found everywhere."
+    },
+    {
+        word: "voracious",
+        clue: "Wanting or devouring great quantities of food."
+    },
+    {
+        word: "wistful",
+        clue: "Having or showing a feeling of vague or regretful longing."
+    },
+    {
+        word: "xenophobia",
+        clue: "Dislike of or prejudice against people from other countries."
+    },
+    {
+        word: "yield",
+        clue: "To produce or provide a natural, agricultural, or industrial product."
+    },
+    {
+        word: "zealot",
+        clue: "A person who is fanatical and uncompromising in pursuit of their religious, political, or other ideals."
+    },
+    {
+        word: "capitulate",
+        clue: "To surrender or give in to an opponent."
+    },
+    {
+        word: "delineate",
+        clue: "To describe or portray precisely."
+    },
+    {
+        word: "ebullient",
+        clue: "Cheerful and full of energy."
+    },
+    {
+        word: "fecund",
+        clue: "Producing or capable of producing offspring or fruit in large quantities."
+    },
+    {
+        word: "grandiose",
+        clue: "Impressive or ambitious in scope."
+    },
+    {
+        word: "histrionic",
+        clue: "Overly theatrical or melodramatic in character or style."
+    },
+    {
+        word: "ineffable",
+        clue: "Too great or extreme to be expressed or described in words."
+    },
+    {
+        word: "jocund",
+        clue: "Cheerful and lighthearted."
+    },
+    {
+        word: "kowtow",
+        clue: "To act in an excessively subservient manner."
+    },
+    {
+        word: "labyrinthine",
+        clue: "Complicated and confusing, like a labyrinth."
+    },
+    {
+        word: "mellifluous",
+        clue: "Flowing with sweetness or honey; pleasant to hear."
+    },
+    {
+        word: "nebulous",
+        clue: "Unclear, vague, or ill-defined."
+    },
+    {
+        word: "opulent",
+        clue: "Ostentatiously rich and luxurious."
+    },
+    {
+        word: "palliate",
+        clue: "To make a disease or its symptoms less severe without removing the cause."
+    },
+    {
+        word: "quixotic",
+        clue: "Extremely idealistic; unrealistic and impractical."
+    },
+    {
+        word: "rambunctious",
+        clue: "Uncontrollably exuberant or boisterous."
+    },
+    {
+        word: "serendipity",
+        clue: "The occurrence and development of events by chance in a happy or beneficial way."
+    },
+    {
+        word: "truncate",
+        clue: "To shorten by cutting off a part."
+    },
+    {
+        word: "ubiquity",
+        clue: "The state of being widespread or ever-present."
+    },
+    {
+        word: "vex",
+        clue: "To make someone feel annoyed, frustrated, or worried."
+    },
+    {
+        word: "whimsical",
+        clue: "Playfully quaint or fanciful, especially in an appealing and amusing way."
+    },
+    {
+        word: "xerophyte",
+        clue: "A plant adapted to grow in conditions of aridity (dryness)."
+    },
+    {
+        word: "yearn",
+        clue: "To have an intense feeling of longing for something."
+    },
+    {
+        word: "zephyr",
+        clue: "A gentle, mild breeze."
+    },
+    {
+        word: "zenith",
+        clue: "The time at which something is most powerful or successful."
+    },
+];
